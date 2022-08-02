503f856d03d60ec35a28e0c7c60b59d917361ea6
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-08-02 23:14:03 +0600

    product section created

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import './Header.css';
-import logo from '../../images/logo.png';
-
-const header = () => {
+import logo from '../../images/logo.png'
+import '../header/Header.css'
+const Header = () => {
     return (
         <div className='header'>
-            <img src={logo} alt="" />
+            <img src={logo} alt=""/>
             <nav>
-                <a href="./shop">Shop</a>
-                <a href="./order">Order Review</a>
-                <a href="./manage">Manage Inventory here</a>
+                <a href="/shop">Shop</a>
+                <a href="/review">Order Review</a>
+                <a href="/manage">Manage Inventory</a>
             </nav>
         </div>
     );
 };
 
-export default header;
+export default Header;
