84ccc0aa8ae21bf3214a40b64d5158bebcce3e9f
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-08-03 12:53:05 +0600

    shipping condition added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,7 +3,15 @@ import './Cart.css';
 
 const Cart = (props) => {
     const cart = props.cart;
-    const total = cart.reduce((total, prd) => total + prd.price , 0)
+    const total = cart.reduce((total, prd) => total + prd.price , 0);
+    var shipping = '12.56';
+
+    if(total > 50){
+        shipping = 0;
+    }
+    else if(total > 20){
+        shipping = 5.46;
+    }
 
     return (
         <div className='cart-info'>
@@ -11,6 +19,7 @@ const Cart = (props) => {
                 <br />
             <h5>Items ordered : {cart.length}</h5>
             <p> Total Price : {total}</p>
+            <p>Shipping cost : {shipping}</p>
         </div>
     );
 };
