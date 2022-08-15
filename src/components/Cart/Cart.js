2762b678bc7707296031c0e173399f209e89e8ba
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-08-16 00:23:38 +0600

    cart quantity problem solved

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -5,7 +5,13 @@ import './Cart.css';
 const Cart = (props) => {
     const cart = props.cart;
     // console.log(cart)
-    const total = cart.reduce((total, prd) => total + prd.price * prd.quantity, 0);
+    // const total = cart.reduce((total, prd) => total + prd.price * prd.quantity, 0);
+    let total = 0;
+    for(let i=0; i<cart.length; i++){
+        const product = cart[i];
+        total = total + product.price + product.quantity;
+    }
+
     var shipping = 0;
 
     if (total > 50) {
