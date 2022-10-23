2c7b2caee456c327c054c0ec14431e85c11e7b46
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-10-23 16:18:31 +0600

    user dashboard created

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,31 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
-import React from 'react';
+import { faShoppingCart, faUser } from '@fortawesome/free-solid-svg-icons';
+import React, { useState } from 'react';
 import logo from '../../images/logo.png';
 import '../header/Header.css';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
+
+    const [dashboard, setDashboard] = useState({
+        isShown : false,
+        display : ''
+    });
+    const handleUserProfile = () =>{
+        if(dashboard.isShown){
+            const newdashboard = {...dashboard}
+            newdashboard.display = 'block';
+            newdashboard.isShown = false;
+            setDashboard(newdashboard)
+        }
+        if(!dashboard.isShown){
+            const newdashboard = {...dashboard}
+            newdashboard.display = '';
+            newdashboard.isShown = true;
+            setDashboard(newdashboard)
+        }
+    }
+    
     return (
         <div className='header'>
             <img src={logo} alt=""/>
@@ -18,6 +38,18 @@ const Header = () => {
                     <Link to="/review" className='nav-link'>
                         <FontAwesomeIcon icon={faShoppingCart} />
                     </Link>
+                </div>
+
+                <div className="user-dashboard">
+                    <div className="user-btn">
+                        <FontAwesomeIcon onClick={handleUserProfile} icon={faUser} />
+                    </div>
+                    {
+                    <ul className='dashboard-menu' style={dashboard}>
+                        <li>Profile</li>
+                        <li>Log Out</li>
+                    </ul>
+                    }
                 </div>
             </nav>
         </div>
